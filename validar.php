91d8229678610b2698bd27db94ee33e98daf9625
maarkalne <marc.alba01@gmail.com>
2022-05-12 18:04:41 +0200

    Oussama7

--- validar.php
+++ validar.php
@@ -1,30 +1,5 @@
 <?php
-$usuario=$_POST['usuario'];
-$contrasenya=$_POST['contrasenya'];
-session_start();
-$_SESSION['usuario']=$usuario;
-
-$conexion=mysqli_connect("localhost","root","marc123","login");
+$servername = "localhost";
 
 
-$consulta="SELECT*FROM usuarios where usuario='$usuario' and contrasenya='$contrasenya'";
-$resultat=mysqli_query($conexio,$consulta);
-
-$filas=mysqli_num_rows($resultat);
-if($filas){
-    header("location:home.php"):
-}else{
-    ?>
-    <?php
-    include("index.php");
-    ?>
-    <h1 class="bad"> ERRO LOGIN </h1>
-    <?php
-}
-
-
-mysqli_free_result($resultat)
-mysqli_close($conexio)
-
-
-
+?>
